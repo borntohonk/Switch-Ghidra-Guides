1a0e2dfa2637c7da7923777a4b60d00a0f774863
borntohonk <6264306+borntohonk@users.noreply.github.com>
2024-03-26 13:29:05 +0100

    Update disable_browser_ca_verification_patch.py for 18.0.0

--- scripts/disable_browser_ca_verification_patch.py
+++ scripts/disable_browser_ca_verification_patch.py
@@ -1,5 +1,15 @@
 import re
+import lz4.block
 
+#18.0.0 romfs/nro/netfront/core_2/default/cfi_enabled/webkit_wkc.nro.lz4
+with open('webkit_wkc.nro.lz4', 'rb') as file:
+    input_data = file.read()
+    decompressed = lz4.block.decompress(input_data)
+    decompressed_browser_file = open('uncompressed_browser_ssl.nro', 'wb')
+    decompressed_browser_file.write(decompressed)
+    decompressed_browser_file.close()
+
+    
 def get_build_id():
     with open('uncompressed_browser_ssl.nro', 'rb') as f:
         f.seek(0x40)
@@ -7,8 +17,8 @@ def get_build_id():
 
 with open('uncompressed_browser_ssl.nro', 'rb') as fi:
     read_data = fi.read()
-    result = re.search(rb'\x48\x00\x80\x52\xE2\x13\x88\x1A\xE8\x03.\x2A\x21\x0A\x80\x52.\x01\x00\x12..\x04\x94..\x41\xF9\x01\x08\x80\x52\xE2\x03', read_data)
-    patch1 = '%08X%s%s' % (result.start(), '0001', '08')
+    result = re.search(rb'\x72\x48\x00\x80\x52\xe2\x13\x88\x1a', read_data)
+    patch1 = '%08X%s%s' % (result.start() + 0x1, '0004', 'E8031F2A')
     patch2 = '%08X%s%s' % (result.end(), '0001', '1F')
     text_file = open(get_build_id() + '.ips', 'wb')
     print('browser-ssl build-id: ' + get_build_id())
