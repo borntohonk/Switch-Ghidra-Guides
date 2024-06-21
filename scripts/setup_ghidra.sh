99904cfe55b68bc41635f480cfee810557c839f9
borntohonk <6264306+borntohonk@users.noreply.github.com>
2024-06-21 10:42:14 +0200

    Correct references for OJDK borntohonk/Switch-Ghidra-Guides#9

--- scripts/setup_ghidra.sh
+++ scripts/setup_ghidra.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd $HOME/ && \
-wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz && \
-mkdir ojdk21 && \
-tar xvzf OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz -C ojdk21 && \
-rm OpenJDK21U-jdk_x64_linux_hotspot_21.0.2_13.tar.gz && \
-export JAVA_HOME=$HOME/ojdk21/jdk-21.0.2+13 && \
-export PATH="$HOME/ojdk21/jdk-21.0.2+13 /bin:$PATH" && \
-echo "export PATH="$HOME/ojdk21/jdk-21.0.2+13 /bin:$PATH"" >> $HOME/.profile && \
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9%2B9/OpenJDK17U-jdk_x64_linux_hotspot_17.0.9_9.tar.gz && \
+mkdir ojdk17 && \
+tar xvzf OpenJDK17U-jdk_x64_linux_hotspot_17.0.9_9.tar.gz -C ojdk17 && \
+rm OpenJDK17U-jdk_x64_linux_hotspot_17.0.9_9.tar.gz && \
+export JAVA_HOME=$HOME/ojdk17/jdk-17.0.9+9 && \
+export PATH="$HOME/ojdk17/jdk-17.0.9+9 /bin:$PATH" && \
+echo "export PATH="$HOME/ojdk17/jdk-17.0.9+9 /bin:$PATH"" >> $HOME/.profile && \
 wget $(curl -s https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -O ghidra.zip && \
 unzip ghidra.zip -d ghidra && \
 rm ghidra.zip && \
 GHIDRA=$(ls $HOME/ghidra) && \
 echo "alias ghidra=$HOME/ghidra/$GHIDRA/ghidraRun" >> $HOME/.bash_aliases && \
 export GHIDRA_INSTALL_DIR=$HOME/ghidra/$GHIDRA && \
-git clone https://github.com/StevensND/Ghidra-Switch-Loader && \
+git clone https://github.com/Adubbz/Ghidra-Switch-Loader.git && \
 cd Ghidra-Switch-Loader && \
 chmod +x gradlew && \
 ./gradlew && \
