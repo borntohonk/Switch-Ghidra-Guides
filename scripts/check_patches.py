de611f58db3bc8f3fb5605591bb0faf790a4dc08
borntohonk <6264306+borntohonk@users.noreply.github.com>
2025-11-26 13:05:06 +0100

    move comments and patterns to be more consistent

--- scripts/check_patches.py
+++ scripts/check_patches.py
@@ -43,13 +43,30 @@ with open('sorted_firmware/by-type/Data/0100000000000809/romfs/file', 'rb') as f
 
 version = firmware_version
 
-fspattern1 = rb'.\x94.{2}\x00\x36.{9}\x25\x80\x52' # here
-# { "noncasigchk_old2", "0x.94..0036.258052", 2, 0, tbz_cond, nop_patch, nop_applied, true, MAKEHOSVERSION(17,0,0), MAKEHOSVERSION(20,5,0) }, // 17.0.0 - 20.5.0
-# { "noncasigchk_new", "0x.94..0036.........258052", 2, 0, tbz_cond, nop_patch, nop_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY }, // 21.0.0+
-fsoffset1 = 2
-fspattern2 = rb'\x40\xf9.{3}\x94.{2}\x40\xb9.{2}\x00\x12'
-# { "nocntchk2", "0x40f9...94..40b9..0012", 2, 0, bl_cond, ret0_patch, ret0_applied, true, MAKEHOSVERSION(19,0,0), FW_VER_ANY },
-fsoffset2 = 2
+# sys-patch logic explanation:
+# "(49, 0,) +49" (nifm/ctest) from start from the start the string found in it's entirity, as sys-patch tests things stupid, then it reads from 0x0 of the end in decimals where the "head" was placed (49),
+# and the 4th byte from there is what is tested (for most cond checks), the next number determines where from the tested byte is being patched. (should be 0, otherwise its not testing the bytes being patched!)
+# should be noted ams loader patch being (6,2) is different from this general rule of not testing for what is being patched.
+# as it finds searches for "009401C0BE12(6)1F00(2)", testing 4 bytes from from offset (6) for cmp_cond byte of "6B",
+# and then applies the patch of "00", two bytes offset after the testing point (0)1F00(2), turning (6)1F00(2)->01<-6B - cmp w0, w1 -  into (6)1F00(2)->00<-6B - cmp w0, w0
+
+fs_pattern1 = rb'.\x94.{2}\x00\x36.{9}\x25\x80\x52' # here
+# { "noncasigchk_17.0.0_20.5.0", "0x.94..0036.258052", 2, 0, tbz_cond, nop_patch, nop_applied, true, MAKEHOSVERSION(17,0,0), MAKEHOSVERSION(20,5,0) }, // 17.0.0 - 20.5.0
+# { "noncasigchk_21.0.0+", "0x.94..0036.........258052", 2, 0, tbz_cond, nop_patch, nop_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY }, // 21.0.0+
+fs_pattern2 = rb'\x40\xf9.{3}\x94.{2}\x40\xb9.{2}\x00\x12'
+# { "nocntchk_19.0.0+", "0x40f9...94..40b9..0012", 2, 0, bl_cond, ret0_patch, ret0_applied, true, MAKEHOSVERSION(19,0,0), FW_VER_ANY },
+fs_offset = 2
+es_pattern = rb'..\x00....\x97\xa0..\xd1...\x97\xe0\x03\x13\x2a...\xa9'
+# { "es_21.0.0+", "0x..00....97a0..d1...97e003132a...a9", 16, 0, mov2_cond, mov0_patch, mov0_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY }, //21.0.0+
+es_offset = 16
+nifm_pattern = rb'\x14.{11}\x91.{11}\x97.{15}\x14'
+# { "ctest_1.9.0-20.5.0", "14...........91...........97...............14", 41, 0, stp_cond, ctest_patch, ctest_applied, true, MAKEHOSVERSION(19,0,0), MAKEHOSVERSION(20,5,0) }, //19.0.0 - 20.5.0
+# { "ctest_21.0.0+", "14...........91...........97...............14", 49, 0, stp_cond, ctest_patch, ctest_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY }, //21.0.0
+nifm_offset = 49
+nim_pattern = rb'.\x07\x00\x35\x1F\x20\x03\xD5....'
+# { "nim_17.0.0-20.5.0", "0x.0F00351F2003D5", 8, 0, adr_cond, mov2_patch, mov2_applied, true, MAKEHOSVERSION(17,0,0), MAKEHOSVERSION(20,5,0) },
+# { "nim_21.0.0+", "0x.0700351F2003D5", 8, 0, adr_cond, mov2_patch, mov2_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY },
+nim_offset= 8
 patch_magic = "5041544348" # "PATCH"
 eof_magic = "454F46" # "EOF"
 patchvalue1 = "1F2003D5" # FS (nop)
@@ -64,18 +81,17 @@ def get_build_id(nso0):
 
 with open(f'{es_path}', 'rb') as decompressed_es_nso:
     read_data = decompressed_es_nso.read()
-    result = re.search(rb'..\x00....\x97\xa0..\xd1...\x97\xe0\x03\x13\x2a...\xa9', read_data)
-    # { "es4", "0x..00....97a0..d1...97e003132a...a9", 16, 0, mov2_cond, mov0_patch, mov0_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY }, //21.0.0+
+    result = re.search(es_pattern, read_data)
     if not result:
         print(f'(ES) {version} ES offset not found')
         print(f'(ES) Sys-patch for ES string is invalid for: {version}\n')
     else:
         if result.group(0)[19:20] in (bytes([0x2A]), bytes([0x92])): # mov2_cond check
-            offset = '%06X' % (result.start() + 16)
-            decompressed_es_nso.seek(result.start() + 16)
+            offset = '%06X' % (result.start() + es_offset)
+            decompressed_es_nso.seek(result.start() + es_offset)
             es_patch_bytes = decompressed_es_nso.read(0x4).hex().upper()
             es_patch_byte = es_patch_bytes[-2:]
-            patch = '%06X%s%s' % (result.start() + 16, '0004', f'{patchvalue3}')
+            patch = '%06X%s%s' % (result.start() + es_offset, '0004', f'{patchvalue3}')
             if es_patch_byte in ('2A', '92'):
                 print(f'(ES) a "MOV" arm instruction with ending of 0x{es_patch_byte} was found within the pattern')
                 print(f'(ES) Sys-patch for ES string still valid for: {version}')
@@ -93,21 +109,16 @@ with open(f'{es_path}', 'rb') as decompressed_es_nso:
 
 with open(f'{nifm_path}', 'rb') as decompressed_nifm_nso:
     read_data = decompressed_nifm_nso.read()
-    result = re.search(rb'\x14.{11}\x91.{11}\x97.{15}\x14', read_data)
-    # { "ctest2", "14...........91...........97...............14", 41, 0, stp_cond, ctest_patch, ctest_applied, true, MAKEHOSVERSION(19,0,0), MAKEHOSVERSION(20,5,0) }, //19.0.0 - 20.5.0
-    # { "ctest3", "14...........91...........97...............14", 49, 0, stp_cond, ctest_patch, ctest_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY }, //21.0.0
-    # "(49, 0,) +49" from start from the start the string found in it's entirity, as sys-patch tests things stupid, then it reads from 0x0 of the end in decimals where the "head" was placed (49), and the 4th byte from there is what is tested (for most cond checks), the next number determines where from the tested byte is being patched. (should be 0, otherwise its not testing the bytes being patched!)
-    # should be noted ams loader patch being (6,2) is different from this general rule of not testing for what is being patched.
-    # as it finds searches for "009401C0BE12(6)1F00(2)", testing 4 bytes from from offset (6) for cmp_cond byte of "6B" and then applies the patch of "00", two bytes offset after the testing point (0)1F00(2), turning (6)1F00(2)->01<-6B - cmp w0, w1 -  into (6)1F00(2)->00<-6B - cmp w0, w0
+    result = re.search(nifm_pattern, read_data)
     if not result:
         print(f'(NIFM) {version} NIFM offset not found')
         print(f'(NIFM) Sys-patch for NIFM string is invalid for: {version}\n')
     else:
-        offset = '%06X' % (result.start() + 49)
-        decompressed_nifm_nso.seek(result.start() + 49)
+        offset = '%06X' % (result.start() + nifm_offset)
+        decompressed_nifm_nso.seek(result.start() + nifm_offset)
         nifm_patch_bytes = decompressed_nifm_nso.read(0x4).hex().upper() # example for 21.0.0 FD7BBDA9 / stp x29, x30, [sp, #-0x30]!
         nifm_patch_byte = nifm_patch_bytes[-2:]
-        patch = '%06X%s%s' % (result.start() + 49, '0014', f'{patchvalue4}')
+        patch = '%06X%s%s' % (result.start() + nifm_offset, '0014', f'{patchvalue4}')
         if nifm_patch_byte in ('A9'):
             print(f'(NIFM) an "STP" arm instruction with ending of 0x{nifm_patch_byte} was found proceding the pattern')
             print(f'(NIFM) Sys-patch for NIFM string still valid for: {version}')
@@ -123,19 +134,17 @@ with open(f'{nifm_path}', 'rb') as decompressed_nifm_nso:
 
 with open(f'{nim_path}', 'rb') as decompressed_nim_nso:
     read_data = decompressed_nim_nso.read()
-    result = re.search(rb'.\x07\x00\x35\x1F\x20\x03\xD5....', read_data) 
-	# { "nim_old", "0x.0F00351F2003D5", 8, 0, adr_cond, mov2_patch, mov2_applied, true, MAKEHOSVERSION(17,0,0), MAKEHOSVERSION(20,5,0) },
-    # { "nim_new", "0x.0700351F2003D5", 8, 0, adr_cond, mov2_patch, mov2_applied, true, MAKEHOSVERSION(21,0,0), FW_VER_ANY },
+    result = re.search(nim_pattern, read_data)
     if not result:
         print(f'(NIM) {version} NIM offset not found')
         print(f'(NIM) Sys-patch for NIM string is invalid for: {version}\n')
     else:
         if result.group(0)[11:12] == bytes([0x10]): # adr_cond check
-            offset = '%06X' % (result.start() + 8)
-            decompressed_nim_nso.seek(result.start() + 8)
+            offset = '%06X' % (result.start() + nim_offset)
+            decompressed_nim_nso.seek(result.start() + nim_offset)
             nim_patch_bytes = decompressed_nim_nso.read(0x4).hex().upper()
             nim_patch_byte = nim_patch_bytes[-2:]
-            patch = '%06X%s%s' % (result.start() + 8, '0004', f'{patchvalue3}')
+            patch = '%06X%s%s' % (result.start() + nim_offset, '0004', f'{patchvalue3}')
             if nim_patch_byte in ('10'):
                 print(f'(NIM) a "ADR" arm instruction with ending of 0x{nim_patch_byte} was found within the pattern')
                 print(f'(NIM) Sys-patch for NIM string still valid for: {version}')
@@ -153,8 +162,8 @@ with open(f'{nim_path}', 'rb') as decompressed_nim_nso:
 
 with open(decompressed_fat32_path, 'rb') as fat32f:
     read_data = fat32f.read()
-    result1 = re.search(fspattern1, read_data)
-    result2 = re.search(fspattern2, read_data)
+    result1 = re.search(fs_pattern1, read_data)
+    result2 = re.search(fs_pattern2, read_data)
     if not result1:
         print(f'(FS-FAT32) {version} First FS-FAT32 offset not found')
         print(f'(FS-FAT32) Sys-patch for FS-FAT32 noncasigchk_new string is invalid for: {version}\n')
@@ -163,12 +172,12 @@ with open(decompressed_fat32_path, 'rb') as fat32f:
         print(f'(FS-FAT32) Sys-patch for FS-FAT32 nocntchk2 string is invalid for: {version}\n')
     else:
         if result1.group(0)[5:6] == bytes([0x36]) and result2.group(0)[5:6] == bytes([0x94]): # tbz_cond and bl_cond check
-            offset1 = '%06X' % (result1.start() + fsoffset1)
-            offset2 = '%06X' % (result2.start() + fsoffset2)
-            fat32f.seek(result1.start() + fsoffset1)
+            offset1 = '%06X' % (result1.start() + fs_offset)
+            offset2 = '%06X' % (result2.start() + fs_offset)
+            fat32f.seek(result1.start() + fs_offset)
             fat32_patch1_bytes = fat32f.read(0x4).hex().upper()
             fat32_patch1_byte = fat32_patch1_bytes[-2:]
-            fat32f.seek(result2.start() + fsoffset2)
+            fat32f.seek(result2.start() + fs_offset)
             fat32_patch2_bytes = fat32f.read(0x4).hex().upper()
             fat32_patch2_byte = fat32_patch2_bytes[-2:]
             if fat32_patch1_byte in ('36'):
@@ -201,8 +210,8 @@ fat32f.close()
 if os.path.exists(decompressed_exfat_path):
     with open(decompressed_exfat_path, 'rb') as exfatf:
         read_data = exfatf.read()
-        result1 = re.search(fspattern1, read_data)
-        result2 = re.search(fspattern2, read_data)
+        result1 = re.search(fs_pattern1, read_data)
+        result2 = re.search(fs_pattern2, read_data)
         if not result1:
             print(f'(FS-EXFAT) {version} First FS-ExFAT offset not found')
             print(f'(FS-EXFAT) Sys-patch for FS-ExFAT noncasigchk_new string is invalid for: {version}\n')
@@ -211,12 +220,12 @@ if os.path.exists(decompressed_exfat_path):
             print(f'(FS-EXFAT) Sys-patch for FS-ExFAT nocntchk2 string is invalid for: {version}\n')
         else:
             if result1.group(0)[5:6] == bytes([0x36]) and result2.group(0)[5:6] == bytes([0x94]): # tbz_cond and bl_cond check
-                offset1 = '%06X' % (result1.start() + fsoffset1)
-                offset2 = '%06X' % (result2.start() + fsoffset2)
-                exfatf.seek(result1.start() + fsoffset1)
+                offset1 = '%06X' % (result1.start() + fs_offset)
+                offset2 = '%06X' % (result2.start() + fs_offset)
+                exfatf.seek(result1.start() + fs_offset)
                 exfat_patch1_bytes = exfatf.read(0x4).hex().upper()
                 exfat_patch1_byte = exfat_patch1_bytes[-2:]
-                exfatf.seek(result2.start() + fsoffset2)
+                exfatf.seek(result2.start() + fs_offset)
                 exfat_patch2_bytes = exfatf.read(0x4).hex().upper()
                 exfat_patch2_byte = exfat_patch2_bytes[-2:]
                 if exfat_patch1_byte in ('36'):
