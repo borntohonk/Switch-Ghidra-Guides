68d1580bc0251421db840c12c24e1e684cfc2e3e
borntohonk <6264306+borntohonk@users.noreply.github.com>
2025-12-11 10:16:27 +0100

    fix disable_ca_verification_patch.py for 21.0.0+ target

--- scripts/disable_ca_verification_patch.py
+++ scripts/disable_ca_verification_patch.py
@@ -27,13 +27,15 @@ def get_build_id():
 
 with open('ssl.nso0', 'rb') as fi:
     read_data = fi.read()
-    result1 = re.search(rb'\x6a\x00\x80\xd2', read_data)
+    #result1 = re.search(rb'\x6a\x00\x80\xd2', read_data)
+    result1 = re.search(rb'\x08\x00\x80\x12\x69\x12\x05\x91\x7f\x1e\x00\xf9\x68\x42\x00\xb9', read_data) # temp hardcoded bytes for 21.0.0, diff later
+    #result1 ghidra string 08 00 80 12 69 12 05 91 7f 1e 00 f9 68 42 00 b9 08 00 80 d2
     result23 = re.search(rb'\x24\x09\x43\x7a\xa0\x00\x00\x54', read_data)
     result4 = re.search(rb'\x88\x16\x00\x12', read_data)
-    patch1 = '%08X%s%s' % (result1.start(), '0001', '0A')
-    patch2 = '%08X%s%s' % (result23.end() - 4, '0002', '1000')
+    patch1 = '%08X%s%s' % (result1.start() + 16 , '0001', '08') # 0x119A60 in 21.0.0+ - register x8, register x10 before
+    patch2 = '%08X%s%s' % (result23.end() - 4, '0002', '1300') # 1300 in 21.0.0+
     patch3 = '%08X%s%s' % (result23.end() - 1, '0001', '14')
-    patch4 = '%08X%s%s' % (result4.end(), '0004', '08008052')
+    patch4 = '%08X%s%s' % (result4.end() + 4, '0004', '08008052') # +4 in 21.0.0+
     text_file = open(get_build_id() + '.ips', 'wb')
     print('ssl build-id: ' + get_build_id())
     print('disable_ca_verification offsets and patches at: ' + patch1 + patch2 + patch3 + patch4)
